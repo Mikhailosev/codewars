37c1571e8241eb2a8a9d50bb10e83e2d556055e4
Mikhailosev <mikhail.losev2@gmail.com>
2019-03-05 15:41:24 +0200

    orderspls

---
+++ orderspls.js
@@ -0,0 +1,31 @@
+    function order(words) {
+        var newArr=[]
+        newWords=words.split(' ')
+        let k = 1
+        for (let i=0;i<newWords.length;i++) {
+            if (newWords[i].indexOf(k)>-1) {
+                newArr.push(newWords[i]);
+                i=-1
+                k++
+            }
+        }
+        console.log(newArr)
+        return newArr.join(' ')
+    }
+    order("is2 Thi1s T4est 3a")
+   
+   
+   
+    /*function order(words){
+    let i=0
+    let onlyNumb=[]
+    let newArr=[]
+      let newWords=words.split(' ')
+      for (i; i<newWords.length;i++){
+      let regExpNum = /\d+/g;
+      onlyNumb=words.match( regExpNum );
+      onlyNumb.sort();
+      }
+    }
+          order("is2 Thi1s T4est 3a")
+  */
